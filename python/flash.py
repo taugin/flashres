114a3963e0a65a490398610908179c5621df08d2
taugin <taugin@163.com>
2019-01-28 10:54:32 +0800

    1. 增加忽略文件

--- python/flash.py
+++ python/flash.py
@@ -5,21 +5,34 @@ import sys
 import urllib
 import urllib.request
 import random
+import signal
 
 URL_PREFIX = "https://raw.githubusercontent.com/taugin/flashres/master/themeres/"
+
+USER_CANCELED = False
+
 def log(msg):
     print("[Logging...] %s" % msg)
+
+def sigint_handler(signum, frame):
+  global USER_CANCELED
+  USER_CANCELED = True
+
+signal.signal(signal.SIGINT, sigint_handler)
+signal.signal(signal.SIGTERM, sigint_handler)
 
 def get_dynamic_list(process_dir):
     log("生成资源列表")
     resdir = process_dir
     filelist = os.listdir(resdir)
+    random.shuffle(filelist)
     res = []
     max_len = 0
     for file in filelist:
         if file.endswith("gif") or file.endswith("mp4"):
             base, ext = os.path.splitext(file)
             resitem = collections.OrderedDict()
+            base = base.replace("_", " ")
             resitem["name"] = base.capitalize()
             name_len = len(resitem["name"])
             if name_len > max_len:
@@ -47,10 +60,15 @@ def is_url_exist(url):
 
 def checkUrl(res_list, max_len):
     log("检查资源列表")
+    global USER_CANCELED
     new_res_list = []
+    err_res_list = []
     cover_url_exist = True
     theme_url_exist = True
     for res_item in res_list:
+        if USER_CANCELED == True:
+            log("用户取消操作")
+            break;
         if res_item != None and "cover_url" in res_item and "theme_url" in res_item:
             name = None
             restype = None
@@ -67,8 +85,11 @@ def checkUrl(res_list, max_len):
             if theme_url_exist == True and cover_url_exist == True:
                 status = "资源存在"
                 new_res_list.append(res_item)
+            else:
+                status = "资源不存在"
+                err_res_list.append(res_item)
             log("资源处理结果 : %-*s [%-5s] -> [%-6s] - [%s]" % (int(max_len), name, restype, status, theme_url))
-    return new_res_list
+    return new_res_list, err_res_list
 
 def sort_res_list(res_list, sort_way):
     sort_string = "降序"
@@ -88,10 +109,10 @@ def sort_res_list(res_list, sort_way):
         random.shuffle(res_list)
     return res_list;
 
-def writeToFile(res_list):
+def writeToFile(res_list, file_name):
     log("写入资源列表")
     output = json.dumps(res_list, sort_keys=False, indent=4)
-    f = open("color_call_config.json", "w")
+    f = open(file_name, "w")
     f.write(output)
     f.close()
 
@@ -107,6 +128,7 @@ if __name__ == "__main__":
         log("Usage : %s resfolder [asc/desc/random]" % os.path.basename(sys.argv[0]))
         sys.exit(0)
     res_list, max_len = get_dynamic_list(process_dir)
-    res_list = checkUrl(res_list, max_len)
+    res_list, err_list = checkUrl(res_list, max_len)
     res_list = sort_res_list(res_list, sort_way)
-    writeToFile(res_list)
+    writeToFile(res_list, "config_list.json")
+    writeToFile(err_list, "error_list.json")
